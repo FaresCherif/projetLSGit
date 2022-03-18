a077ca1da4fb9a2c099874e4543f5a3dc5400d8e
faresCherif <fares.cherif@etu.unilim.fr>
2022-03-18 20:50:15 +0100

    question 1.3 fini

--- aexp.ml
+++ aexp.ml
@@ -6,7 +6,9 @@ type aexp = Int of int
  	     | Sub of aexp * aexp
              | Mult of aexp * aexp ;;
 
-type valuation = (string * int) list;;
+type variable = {nom : string; valeur : int};;
+
+type valuation = variable list;;
 
 let q2_1 = Int(2);;
 let q2_2_1 = Add(Int(2),Int(3));;
@@ -27,13 +29,16 @@ let rec aexp_to_string(e : aexp) : string =
       |Var(s) -> s;;
 ;;
 
-let listVarVal: valuation = [("x",5);("y",9)];;
-let functionFind l va = List.find (fun (v, n) -> v = va) l;;
+let listVarVal = [{ nom = "x"; valeur = 5 }; { nom = "y"; valeur = 9 }];;
 
-let f s : int = 
-	let (_, v) = functionFind listVarVal s in
-	v
+let functionFind l va = List.find (fun (v) -> v.nom = va) l;;
+
+let fonctionVar s valua = 
+	let (v) = functionFind valua s in
+	v.valeur
 ;;
+
+
 
 Printf.printf "%s\n" (aexp_to_string q2_1);;
 Printf.printf "%s\n" (aexp_to_string q2_2_1);;
@@ -48,24 +53,24 @@ Printf.printf "%s\n\n" (aexp_to_string q2_3_5);;
 
 
 
-let rec ainterp(f,e) : int =
+let rec ainterp(e,valuation) : int =
    match e with
-      |Add(e1,e2) -> ainterp(f,e1) + ainterp(f,e2) 
-      |Mult(e1,e2) -> ainterp(f,e1) * ainterp(f,e2)
-      |Sub(e1,e2) -> ainterp(f,e1) - ainterp(f,e2)
+      |Add(e1,e2) -> ainterp(e1,valuation) + ainterp(e2,valuation) 
+      |Mult(e1,e2) -> ainterp(e1,valuation) * ainterp(e2,valuation)
+      |Sub(e1,e2) -> ainterp(e1,valuation) - ainterp(e2,valuation)
       |Int(i) -> i
-      |Var(s) -> f s;;
+      |Var(s) -> fonctionVar s valuation;;
 ;;
 
-Printf.printf "%d\n" (ainterp (f,q2_1));;
-Printf.printf "%d\n" (ainterp (f,q2_2_1));;
-Printf.printf "%d\n" (ainterp (f,q2_2_2));;
-Printf.printf "%d\n" (ainterp (f,q2_2_3));;
-Printf.printf "%d\n" (ainterp (f,q2_3_1));;
-Printf.printf "%d\n" (ainterp (f,q2_3_2));;
-Printf.printf "%d\n" (ainterp (f,q2_3_3));;
-Printf.printf "%d\n" (ainterp (f,q2_3_4));;
-Printf.printf "%d\n\n" (ainterp (f,q2_3_5));;
+Printf.printf "%d\n" (ainterp (q2_1,listVarVal));;
+Printf.printf "%d\n" (ainterp (q2_2_1,listVarVal));;
+Printf.printf "%d\n" (ainterp (q2_2_2,listVarVal));;
+Printf.printf "%d\n" (ainterp (q2_2_3,listVarVal));;
+Printf.printf "%d\n" (ainterp (q2_3_1,listVarVal));;
+Printf.printf "%d\n" (ainterp (q2_3_2,listVarVal));;
+Printf.printf "%d\n" (ainterp (q2_3_3,listVarVal));;
+Printf.printf "%d\n" (ainterp (q2_3_4,listVarVal));;
+Printf.printf "%d\n\n" (ainterp (q2_3_5,listVarVal));;
 
 let rec asubst v expori expfin : aexp =
 match expfin with 
@@ -130,32 +135,29 @@ Printf.printf "%s\n" (bexp_to_string q22_4_2);;
 
 
 
-let listVarValBin: valuation = [("x",7);("y",3)];;
-let fbin s : int =
-        let (_, v) = functionFind listVarValBin s in
-        v
-;;
+let listVarValBin = [{ nom = "x"; valeur = 7 }; { nom = "y"; valeur = 3 }];;
 
-let rec binterp(e : bexp) : bool =
+
+let rec binterp(e,valuation) : bool =
    match e with
-      |And(e1,e2) -> if(binterp(e1)=true && binterp(e2)=true) then true else false
-      |Or(e1,e2) -> if(binterp(e1)=true || binterp(e2)=true) then true else false
-      |Eg(e1,e2) -> if(ainterp(fbin,e1)=ainterp(fbin,e2)) then true else false
-      |Infeg(e1,e2) ->  if(ainterp(fbin,e1)<=ainterp(fbin,e2)) then true else false
+      |And(e1,e2) -> if(binterp(e1,valuation)=true && binterp(e2,valuation)=true) then true else false
+      |Or(e1,e2) -> if(binterp(e1,valuation)=true || binterp(e2,valuation)=true) then true else false
+      |Eg(e1,e2) -> if(ainterp(e1,valuation)=ainterp(e2,valuation)) then true else false
+      |Infeg(e1,e2) ->  if(ainterp(e1,valuation)<=ainterp(e2,valuation)) then true else false
       |Vrai -> true
       |Faux -> false
-      |Neg(e1) -> not(binterp(e1))
+      |Neg(e1) -> not(binterp(e1,valuation))
 ;;
 
-Printf.printf "%B\n" (binterp (q22_1));;
-Printf.printf "%B\n" (binterp (q22_2_1));;
-Printf.printf "%B\n" (binterp (q22_2_2));;
-Printf.printf "%B\n" (binterp (q22_2_3));;
-Printf.printf "%B\n" (binterp (q22_3_1));;
-Printf.printf "%B\n" (binterp (q22_3_2));;
-Printf.printf "%B\n" (binterp (q22_3_3));;
-Printf.printf "%B\n" (binterp (q22_4_1));;
-Printf.printf "%B\n\n" (binterp (q22_4_2));;
+Printf.printf "%B\n" (binterp (q22_1,listVarValBin));;
+Printf.printf "%B\n" (binterp (q22_2_1,listVarValBin));;
+Printf.printf "%B\n" (binterp (q22_2_2,listVarValBin));;
+Printf.printf "%B\n" (binterp (q22_2_3,listVarValBin));;
+Printf.printf "%B\n" (binterp (q22_3_1,listVarValBin));;
+Printf.printf "%B\n" (binterp (q22_3_2,listVarValBin));;
+Printf.printf "%B\n" (binterp (q22_3_3,listVarValBin));;
+Printf.printf "%B\n" (binterp (q22_4_1,listVarValBin));;
+Printf.printf "%B\n\n" (binterp (q22_4_2,listVarValBin));;
 
 
 type prog = Repeat of aexp*prog
@@ -166,17 +168,6 @@ type prog = Repeat of aexp*prog
 ;;
 
 
-(**
-let x = ref(2);;
-Printf.printf "%d" !x;;
-x := 3;;
-Printf.printf "%d" !x;;
-let y = 4;;
-Printf.printf "%d" !x;;
-x := y;;
-
-Printf.printf "%d\n" !x;;
-*)
 
 let q23_1= Affect("x",Int(7));;
 let q23_2_1 = Affect("z",Add(Int(3),Int(4)));;
@@ -201,79 +192,61 @@ Printf.printf "%s\n" (prog_to_string (q23_3_1));;
 Printf.printf "%s\n" (prog_to_string (q23_4_1));;
 
 
-let q6= Affect("x",Int(7));;
 
-
-type valuationref = (string * int ref) list;;
-
-
-let listVarValq6: valuationref = [("x",ref 0);("y",ref 9);("z",ref 7)];;
-let fprog s : int =
-        let (_, v) = functionFind listVarValq6 s in
-        !v
+let rec modifier_val  liste nom_variable valeur = match liste with
+    [] -> [{ nom = nom_variable; valeur = valeur }]
+  | var :: reste when var.nom = nom_variable -> [{ nom = nom_variable; valeur = valeur}] @ reste
+  | var :: reste -> [var] @ (modifier_val reste nom_variable valeur)
 ;;
-
-let fprogc s n=
-        let (_, v) = functionFind listVarValq6 s in
-        v:=n
-;;
-
-
 
 let func x = x + 2;;
 
 
-let selfcompose func n =
+let rec selfcompose f nbBoucle =
+  if (nbBoucle = 0) then function x -> x
+  else function x -> f ((selfcompose f (nbBoucle - 1)) x);;
 
-  if n < 0 then failwith "Selfcompose can t work with n negative"
-  else 
-  let compose func1 func2 x =
-    func1 (func2 x)
-  in
-  let rec compose_rec func n =
-    if n = 0
-    then (
-      fun x -> x
-    )
-    else(
-     compose func ( compose_rec func (n-1) )
-    )
-  in
-  compose_rec func n
+
+Printf.printf "%d\n\n"(selfcompose func 10 (0));;
+
+
+let listVarValBin = [{ nom = "x"; valeur = 7 }; { nom = "y"; valeur = 3 }];;
+
+
+let rec exec commande valuation = match commande with
+    Skip -> valuation
+  | Seq (e1, e2) -> exec e2 (exec e1 valuation)
+  | Cond (booleen, e1, e2) -> if ((binterp(booleen,valuation)) = true) then (exec e1 valuation) else (exec e2 valuation)
+  | Repeat (expression, commande) -> snd ((selfcompose (function (commande, valuation) -> (commande, exec commande valuation)) (ainterp(expression,valuation))) (commande, valuation))
+  | Affect (variable, expression) -> modifier_val valuation variable (ainterp(expression,valuation))
 ;;
 
 
-let rec exec(prog,fprog) =
-   match prog with
-      |Affect(e1,e2) -> fprogc e1 (ainterp(fprog,e2)); ""      
-      |Repeat(e1,e2) -> "" (**selfcompose(f,e1,e2)*)    
-      |Skip -> ""
-      |Seq(e1,e2) -> exec(e1,fprog); exec(e2,fprog) ; ""
-      |Cond(e1,e2,e3) -> if binterp(e1) then (exec(e2,fprog)) else (exec(e3,fprog)); "" 
-;;
-Printf.printf "%d\n"(selfcompose func 5 (1));;
 
 
-
-Printf.printf "%d\n" (fprog("x"));;
-
-exec(q23_1,fprog);;
-
-Printf.printf "%d\n" (fprog("x"));;
+let x=exec q23_1 listVarValBin;;
+Printf.printf "%d\n" (fonctionVar "x" x);;
 
 
-let qtest = Cond(Infeg(Var("x"),Int(9)),Affect("x",Int(42)),Affect("x",Add(Var("n"),Int(1))));;
-let qtest2 = Seq(Affect("x",Int(7)),Cond(Eg(Var("x"),Int(7)),Affect("x",Int(42)),Affect("x",Add(Var("n"),Int(1)))));;
+let listRefactor = [];;
+let funRefac num= Seq(Affect("x",Int(num)),Seq(Affect("y",Sub(Var("x"),Int(1))),Repeat(Sub(Var("y"),Int(1)),Seq(Affect("x",Mult(Var("x"),Var("y"))),Affect(("y"),Sub(Var("y"),Int(1)))))));;
+let factoriel num=if(num>1) then fonctionVar "x" (exec (funRefac(num)) listRefactor) else failwith "factoriel d'un nombre inferieur ou egal a 1 impossible";;
+Printf.printf "factoriel 5 : %d\n" (factoriel(5));;
 
-exec(qtest,fprog);;
-
-Printf.printf "%d\n" (fprog("x"));;
-
-exec(qtest2,fprog);;
-
-Printf.printf "%d\n" (fprog("x"));;
+let listFibonnacci = [];;
+let funFibbonacci num = Seq(Seq(Seq(Affect("x",Int(1)),Affect("y",Int(1))),Affect("z",Int(2))),Repeat(Int(num-3),Seq(Affect("x",Var("y")),Seq(Affect("y",Var("z")),Affect("z",Add(Var("x"),Var("y")))))));;
+let fibbonacci num=if(num>3) then fonctionVar "z" (exec (funFibbonacci(num)) listFibonnacci) else if(num=3)then 2 else if(num=2)then 1 else if(num=1) then 1 else  failwith "factoriel d'un nombre inferieur ou egal a 1 impossible";;
+Printf.printf "fibbonacci 8 : %d\n" (fibbonacci(8));;
 
 
+(**
+Printf.printf "%s\n" (exec (q23_2_1));;
+Printf.printf "%s\n" (exec (q23_2_2));;
+Printf.printf "%s\n" (exec (q23_3_1));;
+Printf.printf "%s\n" (exec (q23_4_1));;
+*)
+
+(**
 type tprop = Vrai
 |Faux
 |And of bexp*bexp 
@@ -293,3 +266,5 @@ type bexp = And of bexp*bexp
        | Vrai
        | Faux
 ;; 
+
+*)
