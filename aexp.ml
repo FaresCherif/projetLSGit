eaedcbddf1e7bca024a4d39cc6d684ba7dd6bf56
faresCherif <fares.cherif@etu.unilim.fr>
2022-03-24 16:57:10 +0100

    commentaire 2

--- aexp.ml
+++ aexp.ml
@@ -13,17 +13,19 @@ type aexp = Int of int
 
 
 (**question 2 *)
-let q2_1 = Int(2);;
-let q2_2_1 = Add(Int(2),Int(3));;
-let q2_2_2 = Sub(Int(2),Int(5));;
-let q2_2_3 = Mult(Int(3),Int(6));;
-let q2_3_1 = Add(Int(2),Var("x"));;
-let q2_3_2 = Mult(Int(4),Var("y"));;
-let q2_3_3 = Mult(Mult(Int(3),Var("x")),Var("x"));;
-let q2_3_4 = Add(Mult(Int(5),Var("x")),Mult(Int(7),Var("y"))) ;; 
-let q2_3_5 = Add(Mult(Int(6),Var("x")),Mult(Mult(Int(5),Var("y")),Var("x")));;
+let q2_1 = Int(2);;  (**2*)
+let q2_2_1 = Add(Int(2),Int(3));; (** 2+3 *)
+let q2_2_2 = Sub(Int(2),Int(5));; (** 2-5 *)
+let q2_2_3 = Mult(Int(3),Int(6));; (** 3*6 *)
+let q2_3_1 = Add(Int(2),Var("x"));; (** 2+x *)
+let q2_3_2 = Mult(Int(4),Var("y"));; (** 4*y *)
+let q2_3_3 = Mult(Mult(Int(3),Var("x")),Var("x"));; (** 3*x*x *)
+let q2_3_4 = Add(Mult(Int(5),Var("x")),Mult(Int(7),Var("y"))) ;; (** 5∗x+7∗y *)
+let q2_3_5 = Add(Mult(Int(6),Var("x")),Mult(Mult(Int(5),Var("y")),Var("x")));; (** 6∗x+5∗y∗x *)
 
-(**question 3 - 1 *)
+(**question 3 - 1 
+   Transforme un aexp en string
+*)
 let rec aexp_to_string(e : aexp) : string =
    match e with
       |Add(e1,e2) -> "( " ^ aexp_to_string(e1) ^ " + " ^ aexp_to_string(e2) ^ ")" 
@@ -36,7 +38,9 @@ let rec aexp_to_string(e : aexp) : string =
 
 
 
-(**question 3 - 2 *)
+(**question 3 - 2 
+Affiche l'aexp
+*)
 Printf.printf "aexp_to_string : \n";;
 Printf.printf "%s\n" (aexp_to_string q2_1);;
 Printf.printf "%s\n" (aexp_to_string q2_2_1);;
@@ -59,14 +63,14 @@ type valuation = variable list;;
 
 (**question 5*)
 
-let functionFind l va = List.find (fun (v) -> v.nom = va) l;;
+let functionFind l va = List.find (fun (v) -> v.nom = va) l;; (** cherche un string dans une liste de variable et retourne cette variable *)
 
-let fonctionVar s valua = 
+let fonctionVar s valua =  (** retrourne la valeur d'une variable dans une liste a partir de son nom en string *)
    let (v) = functionFind valua s in
    v.valeur
 ;;
 
-let rec ainterp(e,valuation) : int =
+let rec ainterp(e,valuation) : int = (** calcule la valeur d'un ainterp *)
    match e with
       |Add(e1,e2) -> ainterp(e1,valuation) + ainterp(e2,valuation) 
       |Mult(e1,e2) -> ainterp(e1,valuation) * ainterp(e2,valuation)
@@ -76,8 +80,9 @@ let rec ainterp(e,valuation) : int =
 ;;
 
 (**question 6*)
+let listVarVal = [{ nom = "x"; valeur = 5 }; { nom = "y"; valeur = 9 }];; (** cree une liste *)
 
-let listVarVal = [{ nom = "x"; valeur = 5 }; { nom = "y"; valeur = 9 }];;
+(** affiche l'ainterp *)
 Printf.printf "ainterp : \n";;
 Printf.printf "%d\n" (ainterp (q2_1,listVarVal));;
 Printf.printf "%d\n" (ainterp (q2_2_1,listVarVal));;
@@ -91,7 +96,7 @@ Printf.printf "%d\n\n" (ainterp (q2_3_5,listVarVal));;
 
 (**partie 1.1.3*)
 (**question 7*)
-let rec asubst v expori expfin : aexp =
+let rec asubst v expori expfin : aexp = (**remplace les instance de v dans expfin par expori *)
 match expfin with 
       |Add(e1,e2) -> Add(asubst v expori e1,asubst v expori e2 )
       |Mult(e1,e2) -> Mult(asubst v expori e1,asubst v expori e2 )
@@ -102,7 +107,7 @@ match expfin with
 
 (**question 8*)
 Printf.printf "aexp_to_string et asubst : \n";;
-Printf.printf "%s\n" (aexp_to_string ( asubst "x" (Int(7)) (asubst "y" (Add(Var("z"),Int(2))) q2_1)));;
+Printf.printf "%s\n" (aexp_to_string (** retourne aexp_to_string en fonction de la liste a droite  *) ( asubst "x" (Int(7) (** remplace x par 7 dans la liste retourne a droite*)) (asubst "y" (Add(Var("z"),Int(2))) q2_1) (** remplace y par z+2 dans q2_1*)) );;
 Printf.printf "%s\n" (aexp_to_string ( asubst "x" (Int(7)) (asubst "y" (Add(Var("z"),Int(2))) q2_2_1)));;
 Printf.printf "%s\n" (aexp_to_string ( asubst "x" (Int(7)) (asubst "y" (Add(Var("z"),Int(2))) q2_2_2)));;
 Printf.printf "%s\n" (aexp_to_string ( asubst "x" (Int(7)) (asubst "y" (Add(Var("z"),Int(2))) q2_2_3)));;
@@ -124,17 +129,19 @@ type bexp = And of bexp*bexp
 ;; 
 
 (**question 2*)
-let q22_1= Vrai;;
-let q22_2_1 = And(Vrai,Faux);;
-let q22_2_2 = Neg(Vrai);;
-let q22_2_3 = Or(Vrai,Faux);;
-let q22_3_1 = Eg(Int(2),Int(4));;
-let q22_3_2 = Eg(Add(Int(3),Int(5)),Mult(Int(2),Int(4)));;
-let q22_3_3 = Eg(Mult(Int(2),Var("x")),Add(Var("y"),Int(1)));;
-let q22_4_1 = Infeg(Int(5),Int(7));;
-let q22_4_2 = And(Infeg(Add(Int(8),Int(9)),Mult(Int(4),Int(5))),Infeg(Add(Int(3),Var("x")),Mult(Int(4),Var("y"))));;
+let q22_1= Vrai;; (* Vrai *)
+let q22_2_1 = And(Vrai,Faux);; (* Vrai et Faux *)
+let q22_2_2 = Neg(Vrai);; (* non Vrai *)
+let q22_2_3 = Or(Vrai,Faux);; (* Vrai ou Faux *)
+let q22_3_1 = Eg(Int(2),Int(4));; (* 2=4 *)
+let q22_3_2 = Eg(Add(Int(3),Int(5)),Mult(Int(2),Int(4)));; (* 3+5=2∗4 *)
+let q22_3_3 = Eg(Mult(Int(2),Var("x")),Add(Var("y"),Int(1)));; (* 2∗x=y+1 *)
+let q22_4_1 = Infeg(Int(5),Int(7));; (* 5<=7 *)
+let q22_4_2 = And(Infeg(Add(Int(8),Int(9)),Mult(Int(4),Int(5))),Infeg(Add(Int(3),Var("x")),Mult(Int(4),Var("y"))));; (* (8+9<=4∗5) et (3+x<=4∗y) *)
 
-(**question 3*)
+(**question 3
+transforme un bexp en string
+*)
 let rec bexp_to_string(e : bexp) : string =
    match e with
       |And(e1,e2) -> "( " ^ bexp_to_string(e1) ^ " et " ^ bexp_to_string(e2) ^ " )"      
@@ -146,6 +153,7 @@ let rec bexp_to_string(e : bexp) : string =
       |Neg(e1) -> "non ( " ^ bexp_to_string(e1) ^ " )"
 ;;
 
+(** affiche le bexp_to_string *)
 Printf.printf "bexp_to_string : \n";;
 Printf.printf "%s\n" (bexp_to_string q22_1);;
 Printf.printf "%s\n" (bexp_to_string q22_2_1);;
@@ -160,7 +168,9 @@ Printf.printf "%s\n\n" (bexp_to_string q22_4_2);;
 
 
 (**partie 1.2.2*)
-(**question 4*)
+(**question 4
+Retroune si une expression boolenne est true ou false
+*)
 let rec binterp(e,valuation) : bool =
    match e with
       |And(e1,e2) -> if(binterp(e1,valuation)=true && binterp(e2,valuation)=true) then true else false
@@ -173,8 +183,9 @@ let rec binterp(e,valuation) : bool =
 ;;
 
 (**question 5*)
-let listVarValBin = [{ nom = "x"; valeur = 7 }; { nom = "y"; valeur = 3 }];;
+let listVarValBin = [{ nom = "x"; valeur = 7 }; { nom = "y"; valeur = 3 }];; (* declare une liste *)
 
+(* Affiche la valeur boolenne des expressions *)
 Printf.printf "binterp : \n";;
 Printf.printf "%B\n" (binterp (q22_1,listVarValBin));;
 Printf.printf "%B\n" (binterp (q22_2_1,listVarValBin));;
@@ -197,13 +208,15 @@ type prog = Repeat of aexp*prog
 
 
 (**question 2*)
-let q23_1= Affect("x",Int(7));;
-let q23_2_1 = Affect("z",Add(Int(3),Int(4)));;
-let q23_2_2 = Affect("x",Mult(Int(2),Var("x")));;
-let q23_3_1 = Seq(Affect("n",Int(3)),Cond(Infeg(Var("n"),Int(4)),Affect("n",Add(Mult(Int(2),Var("n")),Int(3))),Affect("n",Add(Var("n"),Int(1)))));;
-let q23_4_1 = Repeat(Int(10),Affect("x",Add(Var("x"),Int(1))));;
+let q23_1= Affect("y",Int(7));; (* y:=7 *)
+let q23_2_1 = Affect("z",Add(Int(3),Int(4)));; (* z:=3+4 *)
+let q23_2_2 = Affect("x",Mult(Int(2),Var("x")));; (* x:=2*x *)
+let q23_3_1 = Seq(Affect("n",Int(3)),Cond(Infeg(Var("n"),Int(4)),Affect("n",Add(Mult(Int(2),Var("n")),Int(3))),Affect("n",Add(Var("n"),Int(1)))));; (* n:=3; if (n<=4) then n:=2*n+3 else n:=n+1 *)
+let q23_4_1 = Repeat(Int(10),Affect("x",Add(Var("x"),Int(1))));; (*  repeat 10 do x:=x+1 od *)
 
-(**question 3*)
+(**question 3
+transforme un prog en string
+*)
 let rec prog_to_string(e : prog) : string =
    match e with
       |Affect(e1,e2) -> e1 ^ " := " ^ aexp_to_string(e2)      
@@ -213,6 +226,8 @@ let rec prog_to_string(e : prog) : string =
       |Cond(e1,e2,e3) -> "if " ^ bexp_to_string(e1) ^ " then " ^ prog_to_string(e2) ^ " else " ^ prog_to_string(e3)
 ;;
 
+
+(* Affiche le prog *)
 Printf.printf "prog_to_string\n";;
 Printf.printf "%s\n" (prog_to_string (q23_1));;
 Printf.printf "%s\n" (prog_to_string (q23_2_1));;
@@ -224,41 +239,50 @@ Printf.printf "%s\n" (prog_to_string (q23_4_1));;
 (**partie 1.3.1*)
 
 (**question 4*)
-let rec selfcompose f nbBoucle =
+let rec selfcompose f nbBoucle = (* fais une boucle un nombre nbBoucle de fois *)
   if (nbBoucle = 0) then function x -> x
   else function x -> f ((selfcompose f (nbBoucle - 1)) x);;
 
 (**question 5*)
-let func x = x + 2;;
-Printf.printf "selfcompose(10) de (x-->x+2) = %d\n\n"(selfcompose func 10 (0));;
+let func x = x + 2;; (** fonction qui ajoute 2 a lui-meme *)
+Printf.printf "selfcompose(10) de (x-->x+2) = %d\n\n"(selfcompose func 10 (0));; (** affiche le resultat d'appele 10 fois la fonction x en donnant comme premier x 0 *)
 
 
 (**question 6*)
-let rec modifier_val  liste nom_variable valeur = match liste with
+let rec modifier_val  liste nom_variable valeur = match liste with (* cherche une variable dans une liste pou lui associer une valeur, la cree si elle n'est pas definie*)
     [] -> [{ nom = nom_variable; valeur = valeur }]
   | var :: reste when var.nom = nom_variable -> [{ nom = nom_variable; valeur = valeur}] @ reste
   | var :: reste -> [var] @ (modifier_val reste nom_variable valeur)
 ;;
 
-let rec exec commande valuation = match commande with
-    Skip -> valuation
-  | Seq (e1, e2) -> exec e2 (exec e1 valuation)
-  | Cond (booleen, e1, e2) -> if ((binterp(booleen,valuation)) = true) then (exec e1 valuation) else (exec e2 valuation)
-  | Repeat (expression, commande) -> snd ((selfcompose (function (commande, valuation) -> (commande, exec commande valuation)) (ainterp(expression,valuation))) (commande, valuation))
-  | Affect (variable, expression) -> modifier_val valuation variable (ainterp(expression,valuation))
+let rec exec commande valuation = match commande with (*execute un algoarithme *)
+    Skip -> valuation (* passe *)
+  | Seq (e1, e2) -> exec e2 (exec e1 valuation) (* fais 2 actions a la suite *)
+  | Cond (booleen, e1, e2) -> if ((binterp(booleen,valuation)) = true) then (exec e1 valuation) else (exec e2 valuation) (* fais une condition*)
+  | Repeat (expression, commande) -> snd ((selfcompose (function (commande, valuation) -> (commande, exec commande valuation)) (ainterp(expression,valuation))) (commande, valuation)) (* fais une boucle*)
+  | Affect (variable, expression) -> modifier_val valuation variable (ainterp(expression,valuation)) (* affecte une valeur a une variable*)
 ;;
 
 
-let listRefactor = [];;
+Printf.printf "prog_to_string\n";;
+let listRefactor = [];; (*declare une liste vide*)
 let funRefac num= Cond(Infeg(Int(2),Int(num)),Seq(Affect("x",Int(num)),Seq(Affect("y",Sub(Var("x"),Int(1))),Repeat(Sub(Var("y"),Int(1)),Seq(Affect("x",Mult(Var("x"),Var("y"))),Affect(("y"),Sub(Var("y"),Int(1))))))),Affect("x",Int(1)));;
-let factoriel num=fonctionVar "x" (exec (funRefac(num)) listRefactor);;
+let factoriel num=fonctionVar "x" (exec (funRefac(num)) listRefactor);; (*calcul la factorielle du num donnee en entree*)
 Printf.printf "factoriel de 5 = %d\n" (factoriel(5));;
 
 let listFibonnacci = [];;
 let funFibbonacci num =Cond(Infeg(Int(3),Int(num)),Seq(Seq(Seq(Affect("x",Int(1)),Affect("y",Int(1))),Affect("z",Int(2))),Repeat(Int(num-3),Seq(Affect("x",Var("y")),Seq(Affect("y",Var("z")),Affect("z",Add(Var("x"),Var("y"))))))), Cond(Eg(Int(num),Int(3)),Affect("z",Int(2)),Affect("z",Int(1))));;
-let fibbonacci num=fonctionVar "z" (exec (funFibbonacci(num)) listFibonnacci);;
+let fibbonacci num=fonctionVar "z" (exec (funFibbonacci(num)) listFibonnacci);; (*calcul la numieme valeur de la suite de fibonnaci (si inferieur a 2 retourne 1) *)
 Printf.printf "8e valeur de fibbonacci = %d\n\n" (fibbonacci(8));;
 
+let rec parc_context liste=if((List.tl liste)=[]) then (List.hd liste).nom ^ " : "^string_of_int((List.hd liste).valeur) else (List.hd liste).nom ^ " : "^string_of_int((List.hd liste).valeur) ^" , "^parc_context (List.tl liste);; (* affiche le contenu d'une liste*)
+(* bonus *)
+let listVarProg = [{ nom = "x"; valeur = 7 }; { nom = "y"; valeur = 3 }];; (* declare une liste *)
+Printf.printf "[ %s ]\n" (parc_context (exec (q23_1) listVarProg));;
+Printf.printf "[ %s ]\n" (parc_context (exec (q23_2_1) listVarProg));;
+Printf.printf "[ %s ]\n" (parc_context (exec (q23_2_2) listVarProg));;
+Printf.printf "[ %s ]\n" (parc_context (exec (q23_3_1) listVarProg));;
+Printf.printf "[ %s ]\n\n" (parc_context (exec (q23_4_1) listVarProg));;
 
 (**partie 1.4.1*)
 (**question 1*)
@@ -273,19 +297,20 @@ type tprop = Vrai
 ;;
 
 (**question 2*)
-let q24_1 = Vrai;;
-let q24_2_1= And(Vrai,Faux);;
-let q24_2_2=Neg(Vrai);;
-let q24_2_3=Or(Vrai,Faux);;
-let q24_2_4=Implique(Faux,Or(Vrai,Faux));;
-let q24_3_1=Eg(Int(2),Int(4));;
-let q24_3_2=Eg(Add(Int(3),Int(5)),Mult(Int(2),Int(4)));;
-let q24_3_3=Eg(Mult(Int(2),Var("x")),Add(Var("y"),Int(1)));;
-let q24_4_1=Infeg(Add(Int(3),Var("x")),Mult(Int(4),Var("y")));;
-let q24_4_2=And(Infeg(Int(5),Int(7)),Infeg(Add(Int(8),Int(9)),Mult(Int(4),Int(5))));;
-let q24_5=Implique(Eg(Var("x"),Int(1)),Infeg(Var("y"),Int(0)));;
+let q24_1 = Vrai;; (* Vrai *)
+let q24_2_1= And(Vrai,Faux);; (* Vrai et faux *)
+let q24_2_2=Neg(Vrai);; (* non vrai *)
+let q24_2_3=Or(Vrai,Faux);; (* vrai ou faux *)
+let q24_2_4=Implique(Faux,Or(Vrai,Faux));; (* faux implique vrai ou faux *)
+let q24_3_1=Eg(Int(2),Int(4));; (* 2=4 *)
+let q24_3_2=Eg(Add(Int(3),Int(5)),Mult(Int(2),Int(4)));; (* 3+5=2*4 *)
+let q24_3_3=Eg(Mult(Int(2),Var("x")),Add(Var("y"),Int(1)));; (* 2*x=y+1 *)
+let q24_4_1=Infeg(Add(Int(3),Var("x")),Mult(Int(4),Var("y")));; (* 3+x <= 4*y *)
+let q24_4_2=And(Infeg(Int(5),Int(7)),Infeg(Add(Int(8),Int(9)),Mult(Int(4),Int(5))));; (* (5<=7) et (8+9<=4∗5) *)
+let q24_5=Implique(Eg(Var("x"),Int(1)),Infeg(Var("y"),Int(0)));; (* (x=1) implique (y<=0) *)
 
 (**question 3*)
+(* transforme un prop en string *)
 let rec prop_to_string(e : tprop) : string =
    match e with
       |And(e1,e2) -> "( " ^ prop_to_string(e1) ^ " et " ^ prop_to_string(e2) ^ " )"      
@@ -298,7 +323,8 @@ let rec prop_to_string(e : tprop) : string =
       |Implique(e1,e2)->"( " ^prop_to_string(e1)^ " -> "^ prop_to_string(e2) ^ " )"
 ;;
 
-Printf.printf "prog_to_string : \n";;
+(* affiche les prop *)
+Printf.printf "prop_to_string : \n";;
 Printf.printf "%s\n" (prop_to_string q24_1);;
 Printf.printf "%s\n" (prop_to_string q24_2_1);;
 Printf.printf "%s\n" (prop_to_string q24_2_2);;
@@ -312,7 +338,9 @@ Printf.printf "%s\n" (prop_to_string q24_4_2);;
 Printf.printf "%s\n\n" (prop_to_string q24_5);;
 
 (**partie 1.4.2*)
-(**question 4*)
+(**question 4
+retrourne si un prop est true ou false
+*)
 let rec pinterp(e,valuation) : bool =
    match e with
       |And(e1,e2) -> if(pinterp(e1,valuation)=true && pinterp(e2,valuation)=true) then true else false
@@ -325,7 +353,9 @@ let rec pinterp(e,valuation) : bool =
       |Implique(e1,e2) -> pinterp(Or(Neg(e1),e2),valuation)
 ;;
 
-(**question 5*)
+(**question 5
+Affiche si les prop donnees sont true ou false
+*)
 let listVarValBin = [{ nom = "x"; valeur = 7 }; { nom = "y"; valeur = 3 }];;
 Printf.printf "pinterp : \n";;
 Printf.printf "%B\n" (pinterp(q24_1,listVarValBin));;
@@ -341,7 +371,9 @@ Printf.printf "%B\n" (pinterp(q24_4_2,listVarValBin));;
 Printf.printf "%B\n\n" (pinterp(q24_5,listVarValBin));;
 
 (**partie 1.4.3*)
-(**question 6*)
+(**question 6
+remplace dans un prop expfin les apparance de v par expori
+*)
 
 let rec psubst v expori expfin : tprop =
 match expfin with 
@@ -357,7 +389,7 @@ match expfin with
 
 (**question 7*)
 Printf.printf "prog_to_string : \n";;
-Printf.printf "%s\n" (prop_to_string ( psubst "x" (Mult(Int(3),Var("y"))) (psubst "y" (Add(Var("k"),Int(2))) q24_1)));;
+Printf.printf "%s\n" (prop_to_string (*affiche le prop*) ( psubst "x" (Mult(Int(3),Var("y") (* dans la liste retourne a droite remplace x par 3*y *) )) (psubst "y" (Add(Var("k"),Int(2))) q24_1) (* dans la liste remplace y par k+2 *) ));;
 Printf.printf "%s\n" (prop_to_string ( psubst "x" (Mult(Int(3),Var("y"))) (psubst "y" (Add(Var("k"),Int(2))) q24_2_1)));;
 Printf.printf "%s\n" (prop_to_string ( psubst "x" (Mult(Int(3),Var("y"))) (psubst "y" (Add(Var("k"),Int(2))) q24_2_2)));;
 Printf.printf "%s\n" (prop_to_string ( psubst "x" (Mult(Int(3),Var("y"))) (psubst "y" (Add(Var("k"),Int(2))) q24_2_3)));;
@@ -382,7 +414,7 @@ let q94_4 valuation= {pre=And(Eg(Var("in"),Int(5)),Eg(Var("out"),Int(1)));exp=Se
 
 (**partie 1.4.5*)
 (**question 10*)
-let htvalid_test exp valuation : bool= if pinterp(exp.pre,valuation) then (if(pinterp(exp.post,(exec exp.exp valuation))) then true else false ) else false;;
+let htvalid_test exp valuation : bool= if pinterp(exp.pre,valuation) then (if(pinterp(exp.post,(exec exp.exp valuation))) then true else false ) else false;; (* verifie si les pre et post conditions sont vrai*)
 
 (** bonus : retour pour x=2 , r=5 , in=5, out=1*)
 let listVarHoare = [{ nom = "x"; valeur = 2 };{nom="r";valeur=5};{nom="in";valeur=5};{nom="out";valeur=1}];;
@@ -401,7 +433,9 @@ type conclu = Form of tprop
 
 type goal = {context : cont list ;conclusion : conclu};;
 
-(**question 2*)
+(**question 2
+valeur donnee au hasard
+*)
 let p : tprop =Vrai;;
 let q : tprop =Faux;;
 let r : tprop =Vrai;;
@@ -411,16 +445,17 @@ let p2_q2_2={context=[];conclusion=Htri{pre=Eg(Var("x"),Int(-3));exp=Cond(Infeg(
 
 (**question 3*)
 
-let rec trip_to_string(e : hoare_triple) : string =prop_to_string(e.pre)^" "^prog_to_string(e.exp)^" "^prop_to_string(e.post);;
-let print_conclusion(e : conclu) : string =
+let rec trip_to_string(e : hoare_triple) : string =prop_to_string(e.pre)^" "^prog_to_string(e.exp)^" "^prop_to_string(e.post);; (* Transforme un tripler de hoare en string*)
+let print_conclusion(e : conclu) : string = (* retourne le string de la conclusion *)
    match e with
       |Form(e) -> prop_to_string(e)
       |Htri(e) -> trip_to_string(e)
 ;;
 
-let rec parc_context liste=if((List.tl liste)=[]) then (List.hd liste).eti ^ " : "^prop_to_string((List.hd liste).formu) else (List.hd liste).eti ^ " : "^prop_to_string((List.hd liste).formu) ^"\n"^(parc_context (List.tl liste));;
-let print_goal(e : goal)=(if((e.context)!=[]) then (parc_context(e.context)) else "" )^"\n=====================\n"^print_conclusion(e.conclusion);;
+let rec parc_context liste=if((List.tl liste)=[]) then (List.hd liste).eti ^ " : "^prop_to_string((List.hd liste).formu) else (List.hd liste).eti ^ " : "^prop_to_string((List.hd liste).formu) ^"\n"^(parc_context (List.tl liste));; (* parcours un liste de context et en retourne un string*)
+let print_goal(e : goal)=(if((e.context)!=[]) then (parc_context(e.context)) else "" )^"\n=====================\n"^print_conclusion(e.conclusion);; (* concatene le contexte et la conclusion d'un goal *)
 
+(* Affiche un goal *)
 Printf.printf "print_goal : \n";;
 Printf.printf "%s\n\n"(print_goal(p2_q2_1));;
 Printf.printf "%s\n\n\n"(print_goal(p2_q2_2));;
