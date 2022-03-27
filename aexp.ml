f79dda760726b4e2033943d47c80baf1e88be57f
maximax64 <61144909+maximax64@users.noreply.github.com>
2022-03-27 18:27:41 +0200

    ajout readme et ajout commentaires

--- aexp.ml
+++ aexp.ml
@@ -3,7 +3,9 @@
 
 
 (**partie 1.1.1*)
-(**question 1 *)
+(* question 1 
+   défini le type aexp pour les expressions arithmétiques 
+*)
 type aexp = Int of int
         | Var of string
              | Add of aexp * aexp
@@ -12,7 +14,9 @@ type aexp = Int of int
 
 
 
-(**question 2 *)
+(* question 2 
+   test du type aexp sur des expressions
+*)
 let q2_1 = Int(2);;  (**2*)
 let q2_2_1 = Add(Int(2),Int(3));; (** 2+3 *)
 let q2_2_2 = Sub(Int(2),Int(5));; (** 2-5 *)
@@ -23,7 +27,7 @@ let q2_3_3 = Mult(Mult(Int(3),Var("x")),Var("x"));; (** 3*x*x *)
 let q2_3_4 = Add(Mult(Int(5),Var("x")),Mult(Int(7),Var("y"))) ;; (** 5∗x+7∗y *)
 let q2_3_5 = Add(Mult(Int(6),Var("x")),Mult(Mult(Int(5),Var("y")),Var("x")));; (** 6∗x+5∗y∗x *)
 
-(**question 3 - 1 
+(* question 3 - 1 
    Transforme un aexp en string
 *)
 let rec aexp_to_string(e : aexp) : string =
@@ -38,8 +42,8 @@ let rec aexp_to_string(e : aexp) : string =
 
 
 
-(**question 3 - 2 
-Affiche l'aexp
+(* question 3 - 2 
+   Affiche l'aexp
 *)
 Printf.printf "aexp_to_string : \n";;
 Printf.printf "%s\n" (aexp_to_string q2_1);;
@@ -54,15 +58,18 @@ Printf.printf "%s\n" (aexp_to_string q2_3_4);;
 Printf.printf "%s\n\n" (aexp_to_string q2_3_5);;
 
 
-(**partie 1.1.1*)
-(**question 4*)
+(**partie 1.1.2*)
+(* question 4 
+   défini le type valuation pour représenter les valeurs des variables 
+*)
 type variable = {nom : string; valeur : int};;
 
 type valuation = variable list;;
 
 
-(**question 5*)
-
+(* question 5
+   défini fonction ainterp afin de renvoyer la valeur d'une expression arithmétique et d'une valuation
+ *)
 let functionFind l va = List.find (fun (v) -> v.nom = va) l;; (** cherche un string dans une liste de variable et retourne cette variable *)
 
 let fonctionVar s valua =  (** retrourne la valeur d'une variable dans une liste a partir de son nom en string *)
@@ -79,10 +86,12 @@ let rec ainterp(e,valuation) : int = (** calcule la valeur d'un ainterp *)
       |Var(s) -> fonctionVar s valuation;;
 ;;
 
-(**question 6*)
+(* question 6 
+   test d'affichage de sortie de la fonction ainterp
+*)
 let listVarVal = [{ nom = "x"; valeur = 5 }; { nom = "y"; valeur = 9 }];; (** cree une liste *)
 
-(** affiche l'ainterp *)
+(* affiche l'ainterp *)
 Printf.printf "ainterp : \n";;
 Printf.printf "%d\n" (ainterp (q2_1,listVarVal));;
 Printf.printf "%d\n" (ainterp (q2_2_1,listVarVal));;
@@ -95,7 +104,9 @@ Printf.printf "%d\n" (ainterp (q2_3_4,listVarVal));;
 Printf.printf "%d\n\n" (ainterp (q2_3_5,listVarVal));;
 
 (**partie 1.1.3*)
-(**question 7*)
+(* question 7 
+   défini la fonction asubst afin de remplacer une variable d'une expression arithmétique par une autre fonction arithmétique et renvoie l'expression modifiée
+*)
 let rec asubst v expori expfin : aexp = (**remplace les instance de v dans expfin par expori *)
 match expfin with 
       |Add(e1,e2) -> Add(asubst v expori e1,asubst v expori e2 )
@@ -105,7 +116,9 @@ match expfin with
       |Var(s) -> if(s=v) then expori else Var(s) ;;
 ;;
 
-(**question 8*)
+(* question 8 
+   test de l'affichage de la fonction asbst
+*)
 Printf.printf "aexp_to_string et asubst : \n";;
 Printf.printf "%s\n" (aexp_to_string (** retourne aexp_to_string en fonction de la liste a droite  *) ( asubst "x" (Int(7) (** remplace x par 7 dans la liste retourne a droite*)) (asubst "y" (Add(Var("z"),Int(2))) q2_1) (** remplace y par z+2 dans q2_1*)) );;
 Printf.printf "%s\n" (aexp_to_string ( asubst "x" (Int(7)) (asubst "y" (Add(Var("z"),Int(2))) q2_2_1)));;
@@ -118,7 +131,9 @@ Printf.printf "%s\n" (aexp_to_string ( asubst "x" (Int(7)) (asubst "y" (Add(Var(
 Printf.printf "%s\n\n" (aexp_to_string ( asubst "x" (Int(7)) (asubst "y" (Add(Var("z"),Int(2))) q2_3_5)));;
 
 (**partie 1.2.1*)
-(**question 1*)
+(* question 1 
+   défini le type bexp pour les expressions booléennes 
+*)
 type bexp = Vrai
        | Faux
        | And of bexp*bexp
@@ -129,7 +144,9 @@ type bexp = Vrai
 
 ;; 
 
-(**question 2*)
+(* question 2 
+   test du type bexp sur des expressions
+*)
 let q22_1= Vrai;; (* Vrai *)
 let q22_2_1 = And(Vrai,Faux);; (* Vrai et Faux *)
 let q22_2_2 = Neg(Vrai);; (* non Vrai *)
@@ -140,8 +157,8 @@ let q22_3_3 = Eg(Mult(Int(2),Var("x")),Add(Var("y"),Int(1)));; (* 2∗x=y+1 *)
 let q22_4_1 = Infeg(Int(5),Int(7));; (* 5<=7 *)
 let q22_4_2 = And(Infeg(Add(Int(8),Int(9)),Mult(Int(4),Int(5))),Infeg(Add(Int(3),Var("x")),Mult(Int(4),Var("y"))));; (* (8+9<=4∗5) et (3+x<=4∗y) *)
 
-(**question 3
-transforme un bexp en string
+(* question 3
+   transforme un bexp en string
 *)
 let rec bexp_to_string(e : bexp) : string =
    match e with
@@ -154,7 +171,7 @@ let rec bexp_to_string(e : bexp) : string =
       |Neg(e1) -> "non ( " ^ bexp_to_string(e1) ^ " )"
 ;;
 
-(** affiche le bexp_to_string *)
+(* affiche le bexp_to_string *)
 Printf.printf "bexp_to_string : \n";;
 Printf.printf "%s\n" (bexp_to_string q22_1);;
 Printf.printf "%s\n" (bexp_to_string q22_2_1);;
@@ -169,8 +186,8 @@ Printf.printf "%s\n\n" (bexp_to_string q22_4_2);;
 
 
 (**partie 1.2.2*)
-(**question 4
-Retroune si une expression boolenne est true ou false
+(* question 4
+   Retroune si une expression boolenne est true ou false
 *)
 let rec binterp(e,valuation) : bool =
    match e with
@@ -183,7 +200,9 @@ let rec binterp(e,valuation) : bool =
       |Neg(e1) -> not(binterp(e1,valuation))
 ;;
 
-(**question 5*)
+(* question 5 
+   test de la fonction binterp
+*)
 let listVarValBin = [{ nom = "x"; valeur = 7 }; { nom = "y"; valeur = 3 }];; (* declare une liste *)
 
 (* Affiche la valeur boolenne des expressions *)
@@ -199,7 +218,9 @@ Printf.printf "%B\n" (binterp (q22_4_1,listVarValBin));;
 Printf.printf "%B\n\n" (binterp (q22_4_2,listVarValBin));;
 
 (**partie 1.3.1*)
-(**question 1*)
+(* question 1 
+   définiti le type prog qui va donner la syntaxte abstraite des programmes 
+*)
 type prog = Repeat of aexp*prog
 |Skip
 |Seq of prog*prog
@@ -208,15 +229,17 @@ type prog = Repeat of aexp*prog
 ;;
 
 
-(**question 2*)
+(* question 2 
+   test du type prog sur des expressions
+*)
 let q23_1= Affect("y",Int(7));; (* y:=7 *)
 let q23_2_1 = Affect("z",Add(Int(3),Int(4)));; (* z:=3+4 *)
 let q23_2_2 = Affect("x",Mult(Int(2),Var("x")));; (* x:=2*x *)
 let q23_3_1 = Seq(Affect("n",Int(3)),Cond(Infeg(Var("n"),Int(4)),Affect("n",Add(Mult(Int(2),Var("n")),Int(3))),Affect("n",Add(Var("n"),Int(1)))));; (* n:=3; if (n<=4) then n:=2*n+3 else n:=n+1 *)
 let q23_4_1 = Repeat(Int(10),Affect("x",Add(Var("x"),Int(1))));; (*  repeat 10 do x:=x+1 od *)
 
-(**question 3
-transforme un prog en string
+(* question 3
+   transforme un prog en string
 *)
 let rec prog_to_string(e : prog) : string =
    match e with
@@ -237,19 +260,24 @@ Printf.printf "%s\n" (prog_to_string (q23_3_1));;
 Printf.printf "%s\n" (prog_to_string (q23_4_1));;
 
 
-(**partie 1.3.1*)
-
-(**question 4*)
+(**partie 1.3.2*)
+(* question 4 
+   défini la fonction selfcompose qui renvoie une fonction avec une composition n fois par elle-même en fonction d'une fonction et d'un entier n en entrée 
+*)
 let rec selfcompose f nbBoucle = (* fais une boucle un nombre nbBoucle de fois *)
   if (nbBoucle = 0) then function x -> x
   else function x -> f ((selfcompose f (nbBoucle - 1)) x);;
 
-(**question 5*)
+(* question 5
+   test de la fonction selfcompose
+*)
 let func x = x + 2;; (** fonction qui ajoute 2 a lui-meme *)
 Printf.printf "selfcompose(10) de (x-->x+2) = %d\n\n"(selfcompose func 10 (0));; (** affiche le resultat d'appele 10 fois la fonction x en donnant comme premier x 0 *)
 
 
-(**question 6*)
+(* question 6
+   défini la fonction exec qui renvoie toutes les valuations finales d'un programme et d'une valuation initiale.
+*)
 let rec modifier_val  liste nom_variable valeur = match liste with (* cherche une variable dans une liste pou lui associer une valeur, la cree si elle n'est pas definie*)
     [] -> [{ nom = nom_variable; valeur = valeur }]
   | var :: reste when var.nom = nom_variable -> [{ nom = nom_variable; valeur = valeur}] @ reste
@@ -264,7 +292,9 @@ let rec exec commande valuation = match commande with (*execute un algoarithme *
   | Affect (variable, expression) -> modifier_val valuation variable (ainterp(expression,valuation)) (* affecte une valeur a une variable*)
 ;;
 
-
+(* question 7
+   test de la fonction exec
+*)
 Printf.printf "prog_to_string\n";;
 let listRefactor = [];; (*declare une liste vide*)
 let funRefac num= Cond(Infeg(Int(2),Int(num)),Seq(Affect("x",Int(num)),Seq(Affect("y",Sub(Var("x"),Int(1))),Repeat(Sub(Var("y"),Int(1)),Seq(Affect("x",Mult(Var("x"),Var("y"))),Affect(("y"),Sub(Var("y"),Int(1))))))),Affect("x",Int(1)));;
@@ -286,7 +316,9 @@ Printf.printf "[ %s ]\n" (parc_context (exec (q23_3_1) listVarProg));;
 Printf.printf "[ %s ]\n\n" (parc_context (exec (q23_4_1) listVarProg));;
 
 (**partie 1.4.1*)
-(**question 1*)
+(* question 1 
+   défini le type tprop pour la syntaxe abstraite des formules logiques
+*)
 type tprop = Prop_Vrai
 |Prop_Faux
 |And of tprop*tprop 
@@ -297,7 +329,9 @@ type tprop = Prop_Vrai
 |Infeg of aexp * aexp
 ;;
 
-(**question 2*)
+(* question 2
+   test du type tprop sur des expressions
+*)
 let q24_1 = Prop_Vrai;; (* Vrai *)
 let q24_2_1= And(Prop_Vrai,Prop_Faux);; (* Vrai et faux *)
 let q24_2_2=Neg(Prop_Vrai);; (* non vrai *)
@@ -310,8 +344,9 @@ let q24_4_1=Infeg(Add(Int(3),Var("x")),Mult(Int(4),Var("y")));; (* 3+x <= 4*y *)
 let q24_4_2=And(Infeg(Int(5),Int(7)),Infeg(Add(Int(8),Int(9)),Mult(Int(4),Int(5))));; (* (5<=7) et (8+9<=4∗5) *)
 let q24_5=Implique(Eg(Var("x"),Int(1)),Infeg(Var("y"),Int(0)));; (* (x=1) implique (y<=0) *)
 
-(**question 3*)
-(* transforme un prop en string *)
+(* question 3
+   défini la fonction prop_to_string afin de convertir un prop en string
+*)
 let rec prop_to_string(e : tprop) : string =
    match e with
       |And(e1,e2) -> "( " ^ prop_to_string(e1) ^ " et " ^ prop_to_string(e2) ^ " )"      
@@ -339,8 +374,8 @@ Printf.printf "%s\n" (prop_to_string q24_4_2);;
 Printf.printf "%s\n\n" (prop_to_string q24_5);;
 
 (**partie 1.4.2*)
-(**question 4
-retrourne si un prop est true ou false
+(* question 4
+   défini la fonction pinterp afin de retrourner si un prop est true ou false
 *)
 let rec pinterp(e,valuation) : bool =
    match e with
@@ -355,8 +390,8 @@ let rec pinterp(e,valuation) : bool =
       
 ;;
 
-(**question 5
-Affiche si les prop donnees sont true ou false
+(* question 5
+   test de la fonction pinterp 
 *)
 let listVarValBin = [{ nom = "x"; valeur = 7 }; { nom = "y"; valeur = 3 }];;
 Printf.printf "pinterp : \n";;
@@ -373,8 +408,8 @@ Printf.printf "%B\n" (pinterp(q24_4_2,listVarValBin));;
 Printf.printf "%B\n\n" (pinterp(q24_5,listVarValBin));;
 
 (**partie 1.4.3*)
-(**question 6
-remplace dans un prop expfin les apparance de v par expori
+(* question 6
+   défini la fonction psubst qui remplace dans un prop expfin les apparance de v par expori
 *)
 
 let rec psubst v expori expfin : tprop =
@@ -389,7 +424,9 @@ match expfin with
       |Implique(e1,e2) -> Implique(psubst v expori e1,psubst v expori e2)
 ;;
 
-(**question 7*)
+(* question 7
+   test de la fonction psubst
+*)
 Printf.printf "prog_to_string : \n";;
 Printf.printf "%s\n" (prop_to_string (*affiche le prop*) ( psubst "x" (Mult(Int(3),Var("y") (* dans la liste retourne a droite remplace x par 3*y *) )) (psubst "y" (Add(Var("k"),Int(2))) q24_1) (* dans la liste remplace y par k+2 *) ));;
 Printf.printf "%s\n" (prop_to_string ( psubst "x" (Mult(Int(3),Var("y"))) (psubst "y" (Add(Var("k"),Int(2))) q24_2_1)));;
@@ -405,20 +442,26 @@ Printf.printf "%s\n\n" (prop_to_string ( psubst "x" (Mult(Int(3),Var("y"))) (psu
 
 
 (**partie 1.4.4*)
-(**question 8*)
+(* question 8
+   défini le type hoare_triple pour représenter un triplet de Hoare
+*)
 type hoare_triple = { pre : tprop;exp : prog; post : tprop};;
 
-(**question 9*)
+(* question 9
+   test du type hoare_triple avec des triplets de Hoare
+*)
 let q94_1 = {pre = Eg(Var("x"),Int(2));exp=Skip;post=Eg(Var("x"),Int(2))};;
 let q94_2 = {pre=Eg(Var("x"),Int(2));exp=Affect("x",Int(3));post=Infeg(Var("x"),Int(3))};;
 let q94_3 = {pre=Prop_Vrai;exp=Cond(Infeg(Var("x"),Int(0)),Affect("r",Sub(Int(0),Var("x"))),Affect("r",Var("x")));post=Infeg(Int(0),Var("r"))};;
 let q94_4 valuation= {pre=And(Eg(Var("in"),Int(5)),Eg(Var("out"),Int(1)));exp=Seq(Affect("out",Int(factoriel(fonctionVar "in" valuation))),Affect("in",Sub(Int(factoriel(fonctionVar "out" valuation)),Int(1))));post=And(Eg(Var("in"),Int(0)),Eg(Var("out"),Int(120)))};;
 
 (**partie 1.4.5*)
-(**question 10*)
+(* question 10
+   défini la fonction htvalid_test afin de savoir si un triplet de hoare est valide pour la valuation d'entrée
+*)
 let htvalid_test exp valuation : bool= if pinterp(exp.pre,valuation) then (if(pinterp(exp.post,(exec exp.exp valuation))) then true else false ) else false;; (* verifie si les pre et post conditions sont vrai*)
 
-(** bonus : retour pour x=2 , r=5 , in=5, out=1*)
+(* bonus : retour pour x=2 , r=5 , in=5, out=1 *)
 let listVarHoare = [{ nom = "x"; valeur = 2 };{nom="r";valeur=5};{nom="in";valeur=5};{nom="out";valeur=1}];;
 Printf.printf "htvalid_test : \n";;
 Printf.printf "%B\n" (htvalid_test q94_1 listVarHoare);;
@@ -427,7 +470,9 @@ Printf.printf "%B\n" (htvalid_test q94_3 listVarHoare);;
 Printf.printf "%B\n\n" (htvalid_test (q94_4 listVarHoare) listVarHoare);;
 
 (**partie 2.1.1*)
-(**question 1*)
+(* question 1 
+   défini le type goal pour représenter le type d'une preuve
+*)
 type cont = {eti : string ; formu : tprop};; 
 type conclu = Form of tprop
             | Htri of hoare_triple
@@ -435,8 +480,8 @@ type conclu = Form of tprop
 
 type goal = {context : cont list ;conclusion : conclu};;
 
-(**question 2
-valeur donnee au hasard
+(* question 2
+   test du type goal 
 *)
 let p : tprop =Prop_Vrai;;
 let q : tprop =Prop_Faux;;
@@ -445,7 +490,9 @@ let r : tprop =Prop_Vrai;;
 let p2_q2_1={context=[{eti="H";formu=Implique(Or(p,q),r)};{eti="H2";formu=p}];conclusion=Form(Or(p,q))};;
 let p2_q2_2={context=[];conclusion=Htri{pre=Eg(Var("x"),Int(-3));exp=Cond(Infeg(Var("x"),Int(0)),Affect("x",Sub(Int(0),Var("x"))),Skip);post=Eg(Var("x"),Int(3))}};;
 
-(**question 3*)
+(* question 3
+   défini la fonction trip_to_string afin d'afficher visuellement un but
+*)
 
 let rec trip_to_string(e : hoare_triple) : string =prop_to_string(e.pre)^" "^prog_to_string(e.exp)^" "^prop_to_string(e.post);; (* Transforme un tripler de hoare en string*)
 let print_conclusion(e : conclu) : string = (* retourne le string de la conclusion *)
@@ -473,8 +520,8 @@ let fresh_ident =
 
 
 (**partie 2.1.2*)
-
-(** Question 4
+(* Question 4
+   voici l'arbre de preuve du triplet 
 
 {(x = y + i - 1) /\ (i <= 10)} c {[i + 1/i](x = y + i - 1)}
 ------------------------------------------------------------------------repeat(i)
@@ -482,7 +529,8 @@ let fresh_ident =
 
 *)
 
-(** Question 5
+(* Question 5
+   preuve du triplet suivant 
 
 {(r = 0) /\ (n = 1)} repeat 5 do r := r + n; n := n + 1 od {(r = 15) /\ (n = 6)}
 
@@ -503,7 +551,9 @@ od
 *)
 
 (**partie 2.1.3*)
-(**question 6*)
+(* question 6 
+   défini le type tactic afin de définir la syntaxte abstraite des tactiques  
+*)
 
 type tactic =
   (* Logique des propositions *)
@@ -531,7 +581,9 @@ type tactic =
 ;;
 
 (**partie 2.2*)
-(* Question 1 *)
+(* Question 1 
+   défini la fonction bool2prop afin de convertir une expression booléenne en formule logique 
+*)
 
 let rec  bool2prop e =
   match e with
@@ -544,7 +596,9 @@ let rec  bool2prop e =
    |Infeg (e1, e2) -> Infeg(e1, e2)
 ;;
 
-(* Question 2 *)
+(* Question 2 
+   défini la fonction apply_tactic afin d'analyser tous les cas de filtrage
+*)
 
 let append_item lst a = lst @ [a]
 
@@ -636,12 +690,16 @@ let prop = Implique(
 
 let goal = ( [], Form prop );;
 
-
- (* Question 3 *)
+(**partie 2.2.1*)
+(* Question 3 
+   test de la fonction apply_tactic
+*)
 let p2_3={context=[];conclusion=Form(Implique(Implique(Or(p,q),r),And(Implique(p,q),Implique(q,r))))};;
 
-
- (* Question 4 *)
+(**partie 2.2.2*)
+(* Question 4 
+    test de la fonction apply_tactic pour prouver des triplets de Hoare
+*)
 let p_4_1={pre=Eg(Var("x"),Int(2));exp=Skip;post=Eg(Var("x"),Int(2))};;
 let p_4_2={pre=Infeg(Add(Var("y"),Int(1)),Int(4));exp=Affect("y",Add(Var("y"),Int(4)));post=Infeg(Var("y"),Int(4))};;
 let p_4_3={pre=Eg(Var("y"),Int(5));exp=Affect("x",Add(Var("y"),Int(1)));post=Eg(Var("x"),Int(6))};;
---
+++ readme.txt
@@ -0,0 +1,30 @@
+### Informations générales : ###
+
+L'objectif de ce projet est de programmer un interpréteur pour un langage impératif simple ainsi 
+qu'un prouveur en logique de Hoare pour ce langage impératif.
+
+Pour le langage impératif, nous devons définir les expressions arithmétiques, les expressions 
+booléennes et les commandes du langage. 
+
+Pour représenter un triplet de Hoare, il faut représenter une précondition, un programme et une 
+postcondition. Les pré et post conditions sont des formules logiques de la mémoire des prédicats.
+
+Ce projet est développé par Farès CHERIF, Thomas FONTENEAU et Maximiliann MARTINS dans le cadre de 
+l'unité d'enseignement Logiciels Sûrs au cours du semestre 2 du Master Conception Logicielle de 
+l'Université de Poitiers 2021-2022.
+
+### Technologies utilisées : ###
+
+Nous avons choisi le langage Ocaml afin de développer l'interpréteur et le prouveur de Hoare.
+
+### Installation : ###
+
+Vous trouverez dans cette archive, le fichier "aexp.ml" représentant la totalité du code.
+Vous pouvez compiler le code par exemple avec l'éditeur Emacs paramétré afin d'ouvrir des 
+fichiers Ocaml (.ml).
+
+
+### Remarque : ###
+
+Chaque partie d’implémentation s’accompagne d’exemples afin de prouver la validité du code et 
+de donner des exemples d’utilisation du langage. 
